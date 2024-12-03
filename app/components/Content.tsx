34af0a3a2464d2945677cb96abeff1709b515767
LolazyX <arsik.789.king@gmail.com>
2024-12-03 14:16:13 +0700

    update

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -34,7 +34,7 @@ type Data = {
 }
 
 export default function Content({search}: Props) {
-    const [previousSearch, setPreviousSearch] = useState('')
+    const [previousSearch, setPreviousSearch] = useState()
     const [weather, setWeather] =  useState<Data>()
     const initWeather = async () => {
         await getWeatherData(search).then((res) => {
