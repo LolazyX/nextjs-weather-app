9fc17474e778952dd3ebe10d05edce69627569a8
LolazyX <arsik.789.king@gmail.com>
2024-12-04 11:42:18 +0700

    update

--- lib/dateBuild.ts
+++ lib/dateBuild.ts
@@ -1,14 +1,13 @@
 export default function dateBuild() {
     const timestamp = Date.now();
-    
-    // แปลงเป็น ISO 8601 (โดยไม่มีมิลลิวินาทีและ Z)
+
     const date = new Date(timestamp);
     const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // เพิ่ม 0 ถ้าหมายเลขเดือนมี 1 หลัก
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
     const day = String(date.getUTCDate()).padStart(2, '0');
     const hours = String(date.getUTCHours()).padStart(2, '0');
     const minutes = String(date.getUTCMinutes()).padStart(2, '0');
     const seconds = String(date.getUTCSeconds()).padStart(2, '0');
 
-    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day}`;
 }
